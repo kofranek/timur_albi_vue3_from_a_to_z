5ac144294641b9ff8268d98a54e20577891d52a0
Hollarka <kofranek@gmail.com>
2022-02-23 01:39:22 +0100

    Komponenta App, interpolece, posluchači událostí

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button v-on:click="addLike">like</button>
+    <button v-on:click="addDislike">dislike</button>
+  </div>
+  <div>
+    <div>likes count: <strong> {{ likes }}</strong></div>
+    <div>dislikes count: <strong> {{ dislikes }}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes++
+    },
+    addDislike() {
+      this.dislikes++
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
